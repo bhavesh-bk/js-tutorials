1447b0f31854bebede49112904db25bc8102b624
bhavesh-bk <bhaveshbk2003@gmail.com>
2024-07-11 16:36:07 +0000

    more datatypes

--- 01_basics/newdt.js
+++ 01_basics/newdt.js
@@ -1,1 +1,15 @@
-f
+let score = "1000abc0"
+
+console.log(typeof score)
+
+console.log(typeof(score));
+let valinNum = Number(score)
+
+console.log( typeof valinNum);
+
+console.log(valinNum);
+
+let login = 1
+
+let boollogin = Boolean(login)
+console.log(boollogin);
