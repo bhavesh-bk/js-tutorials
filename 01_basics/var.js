866e38784a25a09adba5f79de98425ff1d276ed2
bhavesh-bk <bhaveshbk2003@gmail.com>
2024-07-11 16:22:25 +0000

    datatypes

---
+++ 01_basics/var.js
@@ -0,0 +1,10 @@
+const accId=1443332
+let accEmail="bhavesh86@gmail.com"
+var accPassword="1233"
+accCity="Bangalore"
+
+accEmail="jetsky"
+accPassword="5000"
+accCity="pune"
+let accno
+console.table([accCity,accEmail,accPassword,accno]);
