a5400e3a66c1f575f611974d91e74cd6ac2673da
bhavesh-bk <bhaveshbk2003@gmail.com>
2024-09-25 15:12:29 +0000

    new-commit

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,21 @@
+const score = 40
+
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toFixed(3))
+
+const ono = 123.49557
+// console.log(ono.toPrecision(3))
+const huns = 10000000
+// console.log(huns.toLocaleString());
+
+console.log(Math);
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
