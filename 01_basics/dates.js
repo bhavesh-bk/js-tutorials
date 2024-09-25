57dc6ada85c8636adc166a0880bf0e3792a71f25
bhavesh-bk <bhaveshbk2003@gmail.com>
2024-09-25 16:41:52 +0000

    new-commit-2.0

---
+++ 01_basics/dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+
+let myCreatedDate = new Date("01-13-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+console.log(myDate.getMonth());
+
+myDate.toLocaleDateString()
+
